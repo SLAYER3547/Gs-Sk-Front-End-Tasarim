c3bfdc01b2a5025d0046edf7ca8baaabcf411dc8
SLAYER3547 <73660057+SLAYER3547@users.noreply.github.com>
2023-06-06 12:08:58 +0300

    GS SK FRONT END TASARIM
    Fan yapımı GS SK web sitesi front end tasarımı

---
+++ GS SK/Dynamism.js
@@ -0,0 +1,48 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction2() {
+    var y = document.getElementById("basketballMuseumMenu");
+
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }  
+
+  function myFunction3() {
+    var y = document.getElementById("basketballMuseumMenu");
+    var z = document.getElementById("volleyballMuseumMenu");
+    if (z.style.display === "none") {
+      z.style.display = "block";
+    } else {
+      z.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction5() {
+    var l = document.getElementById("basketballmenu");
+    if (l.style.display === "none") {
+      l.style.display = "block";
+    } else {
+      l.style.display = "none";
+    }
+  } 
+
+  function myFunction6() {
+    var m = document.getElementById("volleyballmenu");
+    if (m.style.display === "none") {
+      m.style.display = "block";
+    } else {
+      m.style.display = "none";
+    }
+  } 
